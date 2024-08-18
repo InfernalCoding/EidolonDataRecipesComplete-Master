d3ac90f70fc2e8e02eee31c5e4342b69cbc9814f
InfernalCoding <137653725+InfernalCoding@users.noreply.github.com>
2024-08-18 15:33:02 -0400

    Update RitualManager.java

--- src/main/java/dev/infernal_coding/eidolonrecipes/rituals/RitualManager.java
+++ src/main/java/dev/infernal_coding/eidolonrecipes/rituals/RitualManager.java
@@ -10,7 +10,6 @@ import elucent.eidolon.ritual.ItemRequirement;
 import elucent.eidolon.util.ColorUtil;
 import net.minecraft.core.Registry;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -18,6 +17,8 @@ import net.minecraftforge.registries.tags.ITag;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static dev.infernal_coding.eidolonrecipes.util.ItemUtil.*;
@@ -116,17 +117,17 @@ public class RitualManager {
         if (type.equals("tag")) {
             itemTag = TagKey.create(Registry.ITEM_REGISTRY, buffer.readResourceLocation());
 
-            if (itemTag != null && !isNecroFocused) {
+            if (!isNecroFocused) {
                 return new Pair<>(new ItemRequirement(itemTag), null);
-            } else if (itemTag != null && isNecroFocused) {
+            } else {
                 return new Pair<>(new ItemRequirement(itemTag), itemTag);
             }
-        } else {
+        } else if (type.equals("item")){
               itemStack = buffer.readItem();
 
-              if (!isNecroFocused && !itemStack.isEmpty()) {
+              if (!isNecroFocused) {
                   return new Pair<>(new ItemRequirement(itemStack), null);
-              } else if (isNecroFocused && !itemStack.isEmpty()) {
+              } else {
                   return new Pair<>(new ItemRequirement(itemStack), itemStack);
               }
           }
@@ -168,41 +169,6 @@ public class RitualManager {
         return new Pair<>(color, isColorPreset);
     }
 
-    static Pair<Integer, Boolean> getColor(FriendlyByteBuf buffer) {
-        int length = buffer.readVarInt();
-        int color = 0;
-        boolean isColorPreset = false;
-
-        if (length == 4) {
-            int colors[] = new int[4];
-            for (int i = 0; i < length; i++) {
-                try {
-                    colors[i] = buffer.readInt();
-                } catch (Exception e) {
-                    colors = null;
-                    break;
-                }
-            }
-            color = colors != null ? ColorUtil.packColor(colors[0], colors[1], colors[2], colors[3]) : 0;
-            isColorPreset = colors != null;
-
-        } else if (length == 3) {
-            int[] colors = new int[3];
-
-            for (int i = 0; i < length; i++) {
-                try {
-                    colors[i] = buffer.readInt();
-                } catch (Exception e) {
-                    colors = null;
-                    break;
-                }
-            }
-            color = colors != null ? ColorUtil.packColor(255, colors[0], colors[1], colors[2]) : 0;
-            isColorPreset = colors != null;
-        }
-        return new Pair<>(color, isColorPreset);
-    }
-
     static Object getBrazierRequirement(JsonObject json) {
         if (JSONUtils.getString(json, "type", "").equals("tag")) {
             return getItemTagFromJson(json.get("tag"));
@@ -231,29 +197,21 @@ public class RitualManager {
         return null;
     }
 
-    static boolean usesNecro(ArrayList<Object> necroReqs, IRequirement requirement) {
-        AtomicBoolean usesNecro = new AtomicBoolean(false);
+    static boolean usesNecro(ArrayList<Object> necroReqs, ItemRequirement requirement, List<IRequirement> reqs) {
+        boolean usesNecro = false;
 
-        necroReqs.forEach(necroRequirement -> {
-            if (requirement instanceof ItemRequirement) {
-                ItemRequirement itemRequirement = (ItemRequirement) requirement;
-
-                if (itemRequirement.getMatch() instanceof ItemStack && necroRequirement.getClass() == itemRequirement.getMatch().getClass()) {
-                    ItemStack itemStack = (ItemStack) necroRequirement;
-                    if (ItemStack.matches(itemStack,  (ItemStack) itemRequirement.getMatch())) {
-                        usesNecro.set(true);
-                    }
-                } else if (necroRequirement instanceof ITag && necroRequirement.getClass() == itemRequirement.getMatch().getClass()) {
-                    ITag<?> iTag = (ITag<?>) necroRequirement;
-                    ITag<?> iTag2 = (ITag<?>) itemRequirement.getMatch();
-
-                    if (iTag.equals(iTag2)) {
-                        usesNecro.set(true);
-                    }
+        for (int j = 0; j < reqs.size(); j++) {
+            for (int i = 0; i < necroReqs.size(); i++) {
+                if (reqs.get(j) == requirement && Objects.equals(requirement.getMatch().toString(), necroReqs.get(i).toString())) {
+                    necroReqs.remove(i);
+                    usesNecro = true;
+                    break;
                 }
             }
-        });
-        return usesNecro.get();
+        }
+
+
+        return usesNecro;
     }
 
     public static class ResultColorPair {
